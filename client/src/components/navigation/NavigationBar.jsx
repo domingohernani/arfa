0ad71348765764e5deebda632ca45532ad1d80fe
domingohernani <hernani.domingo1@gmail.com>
2024-07-04 15:47:25 +0800

    added icons on the navigation

--- client/src/components/navigation/NavigationBar.jsx
+++ client/src/components/navigation/NavigationBar.jsx
@@ -3,6 +3,8 @@ import logo from "../../assets/logos/logo_green.svg";
 import profile from "../../assets/icons/profile.svg";
 import cart from "../../assets/icons/cart.svg";
 import search from "../../assets/icons/search.svg";
+import heart from "../../assets/icons/heart.svg";
+import notif from "../../assets/icons/notif.svg";
 import { Link } from "react-router-dom";
 
 function NavigationBar() {
@@ -28,25 +30,35 @@ function NavigationBar() {
           height="auto"
         />
       </div>
-      <div className="flex items-center justify-end col-start-11 gap-2 ">
-        <button className="flex justify-center gap-2 px-4 bg-transparent focus:outline-none md:px-5 hover:border-arfagreen">
+      <div className="flex items-center justify-end col-start-11 ">
+        <img
+          src={notif}
+          alt="notif"
+          className="w-4 h-auto mx-3 cursor-pointer"
+        />
+        <img
+          src={profile}
+          alt="profile"
+          className="w-4 h-auto mx-3 cursor-pointer"
+        />
+        <button className="flex justify-center gap-2 px-4 bg-transparent focus:outline-none md:px-5">
           <span className="text-xs font-normal text-arfagreen md:text-sm">
             Wishlist
           </span>
           <img
-            src={profile}
-            alt="profile"
-            className="w-3 h-auto cursor-pointer md:w-5"
+            src={heart}
+            alt="wishlist"
+            className="w-3 h-auto cursor-pointer md:w-4"
           />
         </button>
-        <button className="flex justify-center gap-2 px-4 bg-transparent focus:outline-none md:px-5 hover:border-arfagreen">
+        <button className="flex justify-center gap-2 px-4 bg-transparent focus:outline-none md:px-4">
           <span className="text-xs font-normal text-arfagreen md:text-sm">
             Cart
           </span>
           <img
             src={cart}
             alt="cart"
-            className="w-3 h-auto cursor-pointer md:w-5"
+            className="w-3 h-auto cursor-pointer md:w-4"
           />
         </button>
       </div>
