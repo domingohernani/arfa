4d92b2ca82e342e54c00954b2205d093bd4f6ff5
domingohernani <hernani.domingo1@gmail.com>
2024-07-04 15:36:03 +0800

    changed the icon on "add to wishlist"

--- client/src/components/ViewFurniture.jsx
+++ client/src/components/ViewFurniture.jsx
@@ -152,21 +152,14 @@ const ViewFurniture = () => {
                   role="button"
                 >
                   <svg
-                    className="w-5 h-5 -ms-2 me-2"
-                    aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
+                    id="Outline"
+                    viewBox="0 0 24 24"
                     width="24"
                     height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                    className="w-4 h-4 -ms-2 me-2 text-arfablack"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                    />
+                    <path d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z" />
                   </svg>
                   Add to wishlist
                 </a>
