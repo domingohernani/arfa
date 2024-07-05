51d2226893adbb98e0d9a2e48a111b54988067f2
domingohernani <hernani.domingo1@gmail.com>
2024-07-05 22:13:55 +0800

    image rounded-lg was added

--- client/src/pages/shopper/Cart.jsx
+++ client/src/pages/shopper/Cart.jsx
@@ -15,7 +15,7 @@ const displayFurnituresOnCart = () => {
       <div class="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
         <a href="#" class="shrink-0 md:order-1">
           <img
-            class="h-40 w-auto"
+            class="h-40 w-auto rounded-lg"
             src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="image"
           />
