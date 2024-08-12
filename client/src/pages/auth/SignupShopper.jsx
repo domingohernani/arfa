7a90f4cc1eccc487032e0ecd906d333666d34cba
domingohernani <hernani.domingo1@gmail.com>
2024-08-12 22:51:49 +0800

    most logic implemented

--- client/src/pages/auth/SignupShopper.jsx
+++ client/src/pages/auth/SignupShopper.jsx
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 import { doCreateUserWithEmailAndPassword } from "../../firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { db } from "../../firebase/firebase";
-// import useAuthStore from "../../store/useAuthStore";
-import logo from "../../assets/logos/logo_black.svg";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 import { Link, useNavigate } from "react-router-dom";
+import { toast, Toaster } from "react-hot-toast";
+import logo from "../../assets/logos/logo_black.svg";
+import google from "../../assets/icons/google.png";
+import facebook from "../../assets/icons/facebook.png";
+import signupimg from "../../assets/images/signup.png";
 
 const SignupShopper = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const setUser = useAuthStore((state) => state.setUser);
-  // const user = useAuthStore((state) => state.user);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const signup = async (e) => {
     e.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      toast.error("Please enter a valid email address.");
+      return;
+    }
+
+    if (password.length < 6) {
+      toast.error("Password should be at least 6 characters long.");
+      return;
+    }
 
     try {
       const response = await doCreateUserWithEmailAndPassword(
@@ -23,53 +39,40 @@ const SignupShopper = () => {
         password,
         "shopper"
       );
+      toast.success("Account created successfully!");
+      navigate("/");
       // setUser(response);
-      alert("Account Created");
     } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        toast.error("This email is already in use.");
+      } else {
+        toast.error("Error signing up. Please try again later.");
+      }
       console.error("Error signing up:", error);
     }
   };
 
-  const showLoggedUser = async () => {
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    if (user) {
-      try {
-        const userDocRef = doc(db, "users", user.uid);
-        const userDoc = await getDoc(userDocRef);
-        // const userZustand = useAuthStore.getState().user;
-
-        if (!userDoc.exists()) {
-          console.log("No such document!");
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    } else {
-      console.log("No user is currently signed in");
-    }
-  };
-
   return (
-    <div class="flex justify-center flex-row-reverse items-center h-screen">
-      <div class="w-1/2 h-screen hidden lg:block">
+    <div className="flex flex-row-reverse items-center justify-center h-screen">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="hidden w-1/2 h-screen lg:block">
         <img
-          src="https://images.unsplash.com/photo-1567016376408-0226e4d0c1ea?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          srcSet={signupimg}
           alt="Room"
-          class="object-cover w-full h-full"
+          className="object-cover w-full h-full"
         />
       </div>
-      <div class="lg:p-32 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+      <div className="w-full p-8 lg:p-14 md:p-52 sm:20 lg:w-1/2">
         <img src={logo} alt="Arfa" className="py-4 mx-auto w-28" />
-        <p>
+        <h4 className="font-semibold">Sign up</h4>
+        <p className="text-sm">
           Join us today! Create an account to start your shopping journey with
           us. Your next great find is just a few clicks away!
         </p>
-        <form action="#" method="POST">
-          <div class="mb-4">
+        <form onSubmit={signup}>
+          <div className="mb-4">
             <label
-              for="email"
+              htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Email
@@ -77,39 +80,53 @@ const SignupShopper = () => {
             <input
               type="email"
               id="email"
-              placeholder="Enter you email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
               className="bg-gray-50 border pr-6 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-arfagreen focus:border-arfagreen block w-full p-2.5"
+              required
             />
           </div>
-          <div class="mb-4 relative">
+          <div className="relative mb-4">
             <label
-              for="password"
+              htmlFor="password"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Password
             </label>
             <input
-              type="text"
+              type={showPassword ? "text" : "password"}
               id="password"
-              placeholder="Enter you password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
               className="bg-gray-50 border pr-6 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-arfagreen focus:border-arfagreen block w-full p-2.5"
+              required
             />
-            <EyeSlashIcon
-              className="absolute right-0 w-5 h-5 ml-auto mr-1 text-gray-300 cursor-pointer bottom-3 hover:text-gray-500"
-              aria-hidden="true"
-            ></EyeSlashIcon>
-            <EyeIcon
-              className="absolute right-0 hidden w-5 h-5 ml-auto mr-1 text-gray-300 cursor-pointer bottom-3 hover:text-gray-500"
-              aria-hidden="true"
-            ></EyeIcon>
+            {showPassword ? (
+              <EyeSlashIcon
+                onClick={togglePasswordVisibility}
+                className="absolute right-0 w-5 h-5 ml-auto mr-1 text-gray-300 cursor-pointer bottom-3 hover:text-gray-500"
+                aria-hidden="true"
+              />
+            ) : (
+              <EyeIcon
+                onClick={togglePasswordVisibility}
+                className="absolute right-0 w-5 h-5 ml-auto mr-1 text-gray-300 cursor-pointer bottom-3 hover:text-gray-500"
+                aria-hidden="true"
+              />
+            )}
           </div>
           <section className="flex items-center justify-between gap-2 mt-5">
-            <button className="flex-1 px-4 py-2 text-sm font-medium text-white transition rounded-md bg-arfagreen hover:text-white">
-              Sign in
+            <button
+              type="submit"
+              className="flex-1 px-4 py-2 text-sm font-medium text-white transition rounded-md bg-arfagreen hover:text-white"
+            >
+              Sign Up
             </button>
           </section>
         </form>
-        <div class="mt-6 text-blue-500 ">
+        <div className="mt-6 text-blue-500">
           <Link
             className="text-sm underline text-arfagreen"
             to={"/login-shopper"}
@@ -117,40 +134,21 @@ const SignupShopper = () => {
             Already have an account?
           </Link>
         </div>
+        <p className="text-sm text-center">Or sign up using:</p>
+        <section className="flex flex-col items-center gap-3">
+          <div className="flex items-center justify-center w-3/4 gap-3 py-2 border rounded-md cursor-pointer min-w-24">
+            <img src={google} className="w-4 h-4" alt="google" />
+            <span className="text-sm text-blue-600">Continue with Google</span>
+          </div>
+          <div className="flex items-center justify-center w-3/4 gap-3 py-2 border rounded-md cursor-pointer min-w-24">
+            <img src={facebook} className="w-4 h-4" alt="facebook" />
+            <span className="text-sm text-blue-600">
+              Continue with Facebook
+            </span>
+          </div>
+        </section>
       </div>
     </div>
-
-    // <div>
-    //   <div>Shopper Sign Up</div>
-
-    //   <div>
-    //     <label htmlFor="email">Email</label>
-    //     <input
-    //       type="email"
-    //       id="email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       required
-    //     />
-    //   </div>
-    //   <div>
-    //     <label htmlFor="password">Password</label>
-    //     <input
-    //       type="password"
-    //       id="password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       required
-    //     />
-    //   </div>
-    //   <button
-    //     className="px-3 py-2 text-white cursor-pointer bg-arfagreen"
-    //     onClick={signup}
-    //   >
-    //     Sign Up
-    //   </button>
-
-    // </div>
   );
 };
 
