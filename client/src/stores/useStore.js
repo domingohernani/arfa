da26b4f7402c305a8ff96eafd0bc0caa965a8f32
domingohernani <hernani.domingo1@gmail.com>
2024-08-24 15:07:49 +0800

    zustand for better ux implemented

--- client/src/stores/useStore.js
+++ client/src/stores/useStore.js
@@ -1,6 +1,10 @@
 import { create } from "zustand";
 import { furnitureBrowsing } from "./store/furnitureBrowsing";
+import { furnitures } from "./store/furnitures";
+import { orders } from "./store/orders";
 
 export const useStore = create((...a) => ({
   ...furnitureBrowsing(...a),
+  ...furnitures(...a),
+  ...orders(...a),
 }));
