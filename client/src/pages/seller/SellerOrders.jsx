85b3084fc9ec6eaf8736836d2343dfa2a99079d4
domingohernani <hernani.domingo1@gmail.com>
2024-08-26 04:18:11 +0800

    clean coding; removed unused imports and implemented modularity on tables

--- client/src/pages/seller/SellerOrders.jsx
+++ client/src/pages/seller/SellerOrders.jsx
@@ -4,139 +4,16 @@ import { ModuleRegistry } from "@ag-grid-community/core";
 import { AgGridReact } from "@ag-grid-community/react";
 import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-quartz.css";
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { fetchOrdersByShopId } from "../../firebase/orders";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import {
-  EyeIcon,
-  PencilIcon,
-  TrashIcon,
-  EllipsisVerticalIcon,
-} from "@heroicons/react/24/outline";
 import { useStore } from "../../stores/useStore";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { where } from "firebase/firestore";
 import { getOrderStatusStyles } from "../../components/globalFunctions";
+import { CustomRowActions } from "../../components/tables/CustomRowActions";
+import { CustomHoverCopyCell } from "../../components/tables/CustomHoverCopyCell";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
-
-const HoverCopyCellRenderer = ({ value }) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const handleCellClick = () => {
-    setIsVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsVisible(false);
-  };
-
-  const handleCopyClick = async () => {
-    if (isVisible) {
-      try {
-        if (navigator.clipboard && navigator.clipboard.writeText) {
-          await navigator.clipboard.writeText(value);
-          toast.success(`Copied "${value}" to clipboard!`);
-        } else {
-          toast.error("Clipboard API is not available in this browser.");
-        }
-      } catch (err) {
-        toast.error("Failed to copy.");
-      }
-    }
-  };
-
-  return (
-    <div
-      className="relative"
-      onClick={handleCellClick}
-      onMouseLeave={handleMouseLeave}
-    >
-      <span>{value}</span>
-      <div
-        className={`absolute top-0 left-0 z-50 p-2 text-xs text-white bg-gray-800 rounded shadow-lg cursor-pointer w-fit transition-opacity duration-300 ${
-          isVisible ? "opacity-100" : "opacity-0"
-        }`}
-        onClick={handleCopyClick}
-      >
-        {`Copy`}
-      </div>
-    </div>
-  );
-};
-
-const CustomRowActions = ({ data }) => {
-  return (
-    <Menu as="div" className="relative inline-block w-full text-center ">
-      <MenuButton className="inline-flex justify-center text-sm font-medium text-gray-700 bg-transparent hover:border-transparent group hover:text-gray-900">
-        <EllipsisVerticalIcon className="text-arfablack size-4" />
-      </MenuButton>
-      <MenuItems
-        anchor="bottom"
-        className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <EyeIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">View</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 transition-opacity opacity-0 duration-900 group-hover:inline group-hover:opacity-100">
-              ⌘V
-            </span>
-          </button>
-        </MenuItem>
-
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <PencilIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">Edit</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 group-hover:inline">
-              ⌘E
-            </span>
-          </button>
-        </MenuItem>
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <TrashIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">Delete</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 transition-opacity opacity-0 duration-900 group-hover:inline group-hover:opacity-100">
-              ⌘D
-            </span>
-          </button>
-        </MenuItem>
-      </MenuItems>
-    </Menu>
-  );
-};
 
 const SellerOrders = () => {
   const { rowOrdersData, setRowOrdersData } = useStore();
@@ -149,7 +26,7 @@ const SellerOrders = () => {
       flex: 1,
       checkboxSelection: true,
       filter: "agTextColumnFilter",
-      cellRenderer: HoverCopyCellRenderer,
+      cellRenderer: CustomHoverCopyCell,
     },
     {
       headerName: "Date",
@@ -166,7 +43,7 @@ const SellerOrders = () => {
       filter: "agTextColumnFilter",
       valueGetter: (params) =>
         params.data.shopper ? params.data.shopper.email : "--",
-      cellRenderer: HoverCopyCellRenderer,
+      cellRenderer: CustomHoverCopyCell,
     },
     {
       headerName: "Quantity",
--- client/src/pages/seller/SellerTransaction.jsx
+++ client/src/pages/seller/SellerTransaction.jsx
@@ -1,156 +1,23 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { AgGridReact } from "@ag-grid-community/react";
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { ModuleRegistry } from "@ag-grid-community/core";
 import "@ag-grid-community/styles/ag-grid.css";
 import "@ag-grid-community/styles/ag-theme-alpine.css";
 import "@ag-grid-community/styles/ag-theme-quartz.css";
-import CustomButton from "../../components/CustomButton";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import {
-  EyeIcon,
-  PencilIcon,
-  TrashIcon,
-  EllipsisVerticalIcon,
-} from "@heroicons/react/24/outline";
 import { fetchOrdersByShopId } from "../../firebase/orders";
 import { useStore } from "../../stores/useStore";
 import { where } from "firebase/firestore";
 import { getOrderStatusStyles } from "../../components/globalFunctions";
+import { Toaster } from "react-hot-toast";
+import { CustomRowActions } from "../../components/tables/CustomRowActions";
+import { CustomHoverCopyCell } from "../../components/tables/CustomHoverCopyCell";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
-
-const CustomRowActions = ({ data }) => {
-  console.log(data);
-
-  return (
-    <Menu as="div" className="relative inline-block w-full text-center ">
-      <MenuButton className="inline-flex justify-center text-sm font-medium text-gray-700 bg-transparent hover:border-transparent group hover:text-gray-900">
-        <EllipsisVerticalIcon className="text-arfablack size-4" />
-      </MenuButton>
-      <MenuItems
-        anchor="bottom"
-        className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-      >
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <EyeIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">View</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 transition-opacity opacity-0 duration-900 group-hover:inline group-hover:opacity-100">
-              ⌘V
-            </span>
-          </button>
-        </MenuItem>
-
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <PencilIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">Edit</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 group-hover:inline">
-              ⌘E
-            </span>
-          </button>
-        </MenuItem>
-        <MenuItem
-          as="div"
-          className="flex items-center"
-          onClick={() => {
-            alert(data.id);
-          }}
-        >
-          <button className="group flex w-full items-center gap-2 text-arfablack rounded-lg py-1.5 px-3 justify-between">
-            <div className="flex items-center gap-2">
-              <TrashIcon className="text-gray-500 size-4" />
-              <span className="text-sm font-medium cursor-pointer">Delete</span>
-            </div>
-            <span className="hidden text-sm text-gray-500 transition-opacity opacity-0 duration-900 group-hover:inline group-hover:opacity-100">
-              ⌘D
-            </span>
-          </button>
-        </MenuItem>
-      </MenuItems>
-    </Menu>
-  );
-};
 
 const SellerTransaction = () => {
   const { rowTransactionsData, setRowTransactionsData } = useStore();
   const gridRef = useRef();
-
-  const [rowData, setRowData] = useState([
-    {
-      transactionNumber: "TXN1234567890",
-      name: "Jalen Villaflor",
-      location: "Binalonan, Pangasinan",
-      orders: 5,
-      spent: "₱ 5628.80",
-    },
-    {
-      transactionNumber: "TXN0987654321",
-      name: "Jeffrey Saguico",
-      location: "San Manuel, Pangasinan",
-      orders: 12,
-      spent: "₱ 25946.75",
-    },
-    {
-      transactionNumber: "TXN1122334455",
-      name: "Cristine Basada",
-      location: "Laoac, Pangasinan",
-      orders: 6,
-      spent: "₱ 19650.12",
-    },
-    {
-      transactionNumber: "TXN5566778899",
-      name: "Lexi Estandian",
-      location: "Bani, Pangasinan",
-      orders: 3,
-      spent: "₱ 10058.35",
-    },
-    {
-      transactionNumber: "TXN1029384756",
-      name: "Hanz Decano",
-      location: "Urdaneta, Pangasinan",
-      orders: 15,
-      spent: "₱ 32654.03",
-    },
-    {
-      transactionNumber: "TXN5647382910",
-      name: "Arvin Regata",
-      location: "Umingan, Pangasinan",
-      orders: 12,
-      spent: "₱ 45651.79",
-    },
-    {
-      transactionNumber: "TXN1928374655",
-      name: "Joshua Abundiente",
-      location: "Binalonan, Pangasinan",
-      orders: 5,
-      spent: "₱ 16235.56",
-    },
-    {
-      transactionNumber: "TXN8172635409",
-      name: "Charles Inso",
-      location: "Mangaldan, Pangasinan",
-      orders: 7,
-      spent: "₱ 36589.25",
-    },
-  ]);
 
   const columnDefs = useMemo(
     () => [
@@ -159,6 +26,7 @@ const SellerTransaction = () => {
         field: "id",
         flex: 1,
         filter: "agTextColumnFilter",
+        cellRenderer: CustomHoverCopyCell,
       },
       {
         headerName: "Date",
@@ -246,30 +114,32 @@ const SellerTransaction = () => {
         ]),
       ];
       const orders = await fetchOrdersByShopId(filter);
-      console.log(orders);
       setRowTransactionsData(orders);
     };
     fetchOrders();
   }, []);
 
   return (
-    <div
-      className="p-5 ag-theme-quartz"
-      style={{ height: "90%", width: "100%" }}
-    >
-      <AgGridReact
-        rowData={rowTransactionsData}
-        ref={gridRef}
-        columnDefs={columnDefs}
-        defaultColDef={defaultColDef}
-        rowSelection="multiple"
-        suppressRowClickSelection={true}
-        pagination={true}
-        paginationPageSize={10}
-        domLayout="normal"
-        quickFilterText=""
-      />
-    </div>
+    <>
+      <div
+        className="p-5 ag-theme-quartz"
+        style={{ height: "90%", width: "100%" }}
+      >
+        <AgGridReact
+          rowData={rowTransactionsData}
+          ref={gridRef}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          rowSelection="multiple"
+          suppressRowClickSelection={true}
+          pagination={true}
+          paginationPageSize={10}
+          domLayout="normal"
+          quickFilterText=""
+        />
+      </div>
+      <Toaster />
+    </>
   );
 };
 
