d0d20da0d3b636cc4e0403cc93e78cbc89494fdc
domingohernani <hernani.domingo1@gmail.com>
2024-08-31 02:49:07 +0800

    undefined name fixed

--- client/src/components/dynamic/DisplayChat.jsx
+++ client/src/components/dynamic/DisplayChat.jsx
@@ -37,7 +37,7 @@ const DisplayChat = memo(({ chat }) => {
             ></DisplayAvatar>
           )}
           <div className="font-semibold">
-            {`${shopper.firstname} ${shopper.lastname}`}
+            {`${shopper.firstName} ${shopper.lastName}`}
           </div>
         </div>
       </div>
