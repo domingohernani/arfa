286822e568ff15c01625dc36ee514aefc53b0b7a
domingohernani <hernani.domingo1@gmail.com>
2024-09-03 22:15:23 +0800

    responsiveness seller

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -4,15 +4,37 @@ import { SellerSideBar } from "../../components/navigation/SellerSideBar";
 import { ProfileSideBar } from "../../components/navigation/ProfileSideBar";
 import { Outlet } from "react-router-dom";
 import { FooterSection } from "../../components/navigation/FooterSection";
+import { Button, Drawer } from "flowbite-react";
+import { useState } from "react";
+import filter from "../../assets/icons/filter.svg";
+
 const SellerLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => setIsOpen(false);
+
   return (
     <section className="bg-gray-50">
       <section className="bg-white border-b border-gray-300">
         <SellerNavigationBar />
       </section>
 
-      <section className="flex mb-10">
-        <section className="bg-white border-r border-gray-300 w-fit">
+      <section className="my-3 md:hidden">
+        <button
+          type="button"
+          className="p-2 ml-4 -m-2 text-gray-400 bg-transparent rounded-md e hover:border-transparent hover:text-gray-500 sm:ml-6 md:hidden"
+          onClick={() => setIsOpen(true)}
+        >
+          <span className="sr-only">Filters</span>
+          <img src={filter} aria-hidden="true" className="w-5 h-5 " />
+        </button>
+        <Drawer open={isOpen} onClose={handleClose}>
+          <SellerSideBar setIsOpen={setIsOpen}></SellerSideBar>
+        </Drawer>
+      </section>
+
+      <section className="flex md:mb-10">
+        <section className="hidden bg-white border-r border-gray-300 w-fit md:block">
           <SellerSideBar></SellerSideBar>
         </section>
         <section className="flex-1 ">
