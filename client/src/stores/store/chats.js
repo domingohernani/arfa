89745ee44d1d63ce4f93fd02c91b6353a9ef2cac
domingohernani <hernani.domingo1@gmail.com>
2024-09-03 23:45:15 +0800

    zustand implemented

--- client/src/stores/store/chats.js
+++ client/src/stores/store/chats.js
@@ -1,4 +1,6 @@
 export const chats = (set) => ({
+  chats: [],
+  setChats: (data) => set({ chats: data }),  // Correctly set the 'chats' property
   selectedChat: {},
   setSelectedChat: (data) => set({ selectedChat: data }),
 });
