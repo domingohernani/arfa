20a23ca294a5fd106130309734a287b3669235cc
domingohernani <hernani.domingo1@gmail.com>
2024-09-16 22:11:48 +0800

    cart deletion

--- client/src/firebase/wishlist.js
+++ client/src/firebase/wishlist.js
@@ -69,38 +69,3 @@ export const removeFromWishlist = async (userId, furnitureId) => {
   }
 };
 
-export const removeFromCart = async (userId, furnitureId, variant) => {
-  const userRef = doc(db, "users", userId);
-
-  try {
-    const userDoc = await getDoc(userRef);
-
-    if (userDoc.exists()) {
-      const userData = userDoc.data();
-      const cart = userData.cart || [];
-
-      const isInCart = cart.some(
-        (item) => item.furnitureId === furnitureId && item.variant === variant
-      );
-
-      if (isInCart) {
-        await updateDoc(userRef, {
-          cart: arrayRemove({ furnitureId, variant }),
-        });
-        return { success: true, isRemoved: true };
-      } else {
-        return {
-          success: true,
-          isRemoved: false,
-          message: "Item not in cart.",
-        };
-      }
-    } else {
-      console.error("User document does not exist.");
-      return { success: false, isRemoved: false };
-    }
-  } catch (error) {
-    console.error("Error removing item from cart:", error);
-    return { success: false, isRemoved: false };
-  }
-};
