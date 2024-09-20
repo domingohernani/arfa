361b00d55cea74988ece8703494f32de25192774
domingohernani <hernani.domingo1@gmail.com>
2024-09-20 14:18:58 +0800

    order details; almost done

--- client/src/pages/seller/OrderDetails .jsx
+++ client/src/pages/seller/OrderDetails .jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   ArrowLeftIcon,
@@ -6,14 +6,53 @@ import {
   PencilIcon,
 } from "@heroicons/react/20/solid";
 import ItemsOrdered from "../../components/tables/ItemsOrdered";
-import OrderInvoice from "../../components/tables/OrderInvoice";
+import OrderReceipt from "../../components/tables/OrderReceipt";
+import { getOrderById } from "../../firebase/orders";
+import { getUserById } from "../../firebase/user";
+import { getOrderStatusStyles } from "../../components/globalFunctions";
 
 const OrderDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [order, setOrder] = useState();
+  const [customer, setCustomer] = useState();
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        setLoading(true);
+        const orderResult = await getOrderById(id);
+        console.log("order: ", orderResult);
+        setOrder(orderResult);
+        const userResult = await getUserById(orderResult.shopperId);
+        console.log("customer: ", userResult);
+        setCustomer(userResult);
+      } catch (error) {
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrder();
+  }, []);
+
+  if ((loading || !order, !customer)) {
+    return <div>Loading...</div>;
+  }
+
+  const getStatusColor = () => {
+    const { colorClass } = getOrderStatusStyles(order.orderStatus);
+    const bgColorClass = colorClass.includes("-300")
+      ? colorClass.replace("text", "bg").replace("-300", "-50")
+      : colorClass.replace("text", "bg").replace(/-\d{3}/, "-100");
+    return { colorClass, bgColorClass };
+  };
+
   return (
     <section className="m-5">
-      <nav className="flex items-center gap-2 ">
+      <nav className="flex items-center gap-2">
         <div className="p-1 border rounded-sm w-fit">
           <ArrowLeftIcon
             className="w-5 h-5 cursor-pointer"
@@ -29,10 +68,14 @@ const OrderDetails = () => {
       <header className="p-4 mt-4 border rounded-sm">
         <section className="flex flex-wrap items-center justify-between">
           <div className="flex flex-col">
-            <h6 className="font-medium">Order #{id}</h6>
+            <h6 className="font-medium">Order #{order.id}</h6>
             <div className="flex gap-2">
-              <p className="px-2 py-1 text-sm font-medium text-blue-500 rounded-sm bg-blue-50 w-fit">
-                Ready for ship
+              <p
+                className={`px-2 py-1 text-sm font-medium rounded-sm ${
+                  getStatusColor().bgColorClass
+                } w-fit ${getStatusColor().colorClass}`}
+              >
+                {order.orderStatus}
               </p>
               <p className="px-2 py-1 text-sm font-medium text-red-500 rounded-sm bg-red-50 w-fit">
                 Delivery
@@ -72,38 +115,34 @@ const OrderDetails = () => {
           <section className="flex flex-col gap-1 p-4 ">
             <div className="flex justify-between gap-2">
               <div className="flex-1 font-medium">Name: </div>
-              <div className="flex-1 text-gray-600">Hernani Domingo</div>
+              <div className="flex-1 text-gray-600">{`${customer.firstName} ${customer.lastName}`}</div>
             </div>
             <div className="flex justify-between gap-2">
               <div className="flex-1 font-medium">Email: </div>
-              <div className="flex-1 text-gray-600">harry@gmail.com</div>
+              <div className="flex-1 text-gray-600">{customer.email}</div>
             </div>
             <div className="flex justify-between gap-2">
               <div className="flex-1 font-medium">Phone: </div>
-              <div className="flex-1 text-gray-600">09123456789</div>
+              <div className="flex-1 text-gray-600">{customer.phoneNumber}</div>
             </div>
           </section>
         </section>
         <section className="flex-1 border rounded-sm">
           <section className="flex items-center justify-between p-3 py-4 bg-arfagray">
             <span className="font-medium">Shipping Address</span>
-            {/* <button className="flex items-center gap-2 px-4 py-1 text-sm font-medium border border-gray-300 rounded-sm text-arfablack">
-              <PencilIcon className="w-4 h-4 text-gray-400" />
-              Edit
-            </button> */}
           </section>
           <section className="flex flex-col gap-1 p-4 ">
             <p className="text-sm text-gray-600">
-              Alcapa Zone 3, Cabaruan, Urdaneta City, Pangasinan, Region 1
+              {`${customer.location?.street}, ${customer.location?.barangay}, ${customer.location?.city}, ${customer.location?.province}, ${customer.location?.region}`}
             </p>
           </section>
         </section>
       </main>
       <main>
-        <ItemsOrdered />
+        <OrderReceipt />
       </main>
-      <main className="mt-4">
-        <OrderInvoice />
+      <main className="mt-16">
+        <ItemsOrdered orders={order.orderItems} />
       </main>
     </section>
   );
