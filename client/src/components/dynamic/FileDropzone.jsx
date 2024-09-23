de11de168acd2a942a2e6cb376c75d57c75fc37b
domingohernani <hernani.domingo1@gmail.com>
2024-09-24 01:42:44 +0800

    editing ui

---
+++ client/src/components/dynamic/FileDropzone.jsx
@@ -0,0 +1,36 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { CloudArrowUpIcon } from "@heroicons/react/20/solid";
+
+const FileDropzone = ({ text, height }) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Handle the files dropped
+    console.log(acceptedFiles);
+
+    // You can use acceptedFiles to upload or process the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={`border-2 border-dashed rounded-sm ${height} w-full px-2 flex justify-center text-center items-center ${
+        isDragActive ? "border-arfagreen" : "border-gray-300"
+      }`}
+      style={{ cursor: "pointer" }}
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p className="text-sm text-gray-600">Drop the files here...</p>
+      ) : (
+        <div className="flex flex-col items-center">
+          <CloudArrowUpIcon className="w-10 h-w-10" />
+          <p className="text-sm text-gray-600">{text}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileDropzone;
