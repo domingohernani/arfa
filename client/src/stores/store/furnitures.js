fa9647801e1ab993b49b49ff1b5118d603970eef
domingohernani <hernani.domingo1@gmail.com>
2024-09-24 23:29:50 +0800

    variants on zustand

--- client/src/stores/store/furnitures.js
+++ client/src/stores/store/furnitures.js
@@ -1,4 +1,20 @@
 export const furnitures = (set) => ({
   rowFurnituresData: [],
   setRowFurnituresData: (data) => set({ rowFurnituresData: data }),
+
+  variants: [
+    { name: "", imagePaths: [] },
+    { name: "", imagePaths: [] },
+  ],
+  setVariants: (data) => set({ variants: data }),
+  initializeVariants: (currentVariants) =>
+    set({
+      variants:
+        currentVariants.length > 0
+          ? currentVariants
+          : [
+              { name: "", imagePaths: [] },
+              { name: "", imagePaths: [] },
+            ],
+    }),
 });
