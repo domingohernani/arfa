8a4986d30d224125c48592ee258bd600b0620523
domingohernani <dev.hernani.domingo@gmail.com>
2024-10-16 15:03:42 +0800

    tanstack query

--- client/src/components/dynamic/DisplayFurnitures.jsx
+++ client/src/components/dynamic/DisplayFurnitures.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useCallback } from "react";
 import { Link, useParams } from "react-router-dom";
 import DisplayStars from "./DisplayStars";
 import { fetchFurnitureCollection } from "../../firebase/furniture";
+import { useQuery } from "@tanstack/react-query";
 import {
   calculateRatingSummary,
   formatToPeso,
@@ -14,6 +15,7 @@ import noResult from "../../assets/images/no-result.png";
 import { useStore } from "../../stores/useStore";
 import { DisplayFurnituresSkeleton } from "../skeletons/DisplayFurnituresSkeleton";
 
+// Sorting logic remains the same
 const sortFurnitures = (option, furnitures) => {
   if (option === "best-rating") {
     furnitures.sort((a, b) => {
@@ -31,91 +33,90 @@ const sortFurnitures = (option, furnitures) => {
 };
 
 const DisplayFurnitures = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [loading, setLoading] = useState(true);
-  const [furnitures, setFurnitures] = useState([]);
-  const [imageUrls, setImageUrls] = useState([]);
   const { category } = useParams();
-
-  // Zustand values
   const sale = useStore((state) => state.isSaleOnly);
   const newArrival = useStore((state) => state.isNewArrivalsOnly);
   const minPrice = useStore((state) => state.minPrice);
   const maxPrice = useStore((state) => state.maxPrice);
   const sortOption = useStore((state) => state.sortOption);
 
-  // Dimentions
+  // Dimensions
   const width = useStore((state) => state.width);
   const depth = useStore((state) => state.depth);
   const height = useStore((state) => state.height);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        let dataList = [];
-        let filters = [];
-        if (category !== undefined)
-          filters.push(where("category", "==", unSlug(category)));
+  // Data Fetching Function
+  const fetchFurnitures = async () => {
+    let filters = [];
+    if (category !== undefined)
+      filters.push(where("category", "==", unSlug(category)));
 
-        if (sale) filters.push(where("isSale", "==", true));
+    if (sale) filters.push(where("isSale", "==", true));
 
-        if (newArrival) {
-          const now = new Date();
-          const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30));
-          filters.push(where("createdAt", ">=", thirtyDaysAgo));
-        }
+    if (newArrival) {
+      const now = new Date();
+      const thirtyDaysAgo = new Date(now.setDate(now.getDate() - 30));
+      filters.push(where("createdAt", ">=", thirtyDaysAgo));
+    }
 
-        if (minPrice) filters.push(where("discountedPrice", ">=", minPrice));
-        if (maxPrice) filters.push(where("discountedPrice", "<=", maxPrice));
-  
-        if (width) filters.push(where("width", "==", width));
-        if (depth) filters.push(where("depth", "==", depth));
-        if (height) filters.push(where("height", "==", height));
-  
-        if (filters.length > 0) {
-          dataList = await fetchFurnitureCollection("furnitures", filters);
-        } else {
-          dataList = await fetchFurnitureCollection("furnitures");
-        }
-        // Sorting
-        sortFurnitures(sortOption, dataList);
-  
-        setFurnitures(dataList);
-  
-        const urls = await Promise.all(
-          dataList.map(async (item) => {
-            const url = await getImageDownloadUrl(
-              `${item.imagesUrl}/${item.imgPreviewFilename}`
-            );
-            return url;
-          })
+    if (minPrice) filters.push(where("discountedPrice", ">=", minPrice));
+    if (maxPrice) filters.push(where("discountedPrice", "<=", maxPrice));
+
+    if (width) filters.push(where("width", "==", width));
+    if (depth) filters.push(where("depth", "==", depth));
+    if (height) filters.push(where("height", "==", height));
+
+    const dataList = await fetchFurnitureCollection("furnitures", filters);
+
+    // Sorting
+    sortFurnitures(sortOption, dataList);
+
+    // Fetching Image URLs
+    const urls = await Promise.all(
+      dataList.map(async (item) => {
+        const url = await getImageDownloadUrl(
+          `${item.imagesUrl}/${item.imgPreviewFilename}`
         );
-        setImageUrls(urls);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-  
-    fetchData();
-  }, [category, sale, newArrival, minPrice, maxPrice, sortOption, width, depth, height]); 
+        return url;
+      })
+    );
 
-  const onPageChange = useCallback((page) => {
-    setCurrentPage(page);
-  }, []);
+    return { furnitures: dataList, imageUrls: urls };
+  };
+
+  // Use useQuery for fetching data
+  const { data, isLoading, error } = useQuery({
+    queryKey: [
+      "furnitures",
+      {
+        category,
+        sale,
+        newArrival,
+        minPrice,
+        maxPrice,
+        sortOption,
+        width,
+        depth,
+        height,
+      },
+    ],
+    queryFn: fetchFurnitures,
+  });
 
   const displaySkeleton = useCallback((numSkeletons) => {
-    return <DisplayFurnituresSkeleton numSkeletons={numSkeletons} />;
+    return (
+      <section className="bg-white md:pl-8 text-arfablack">
+        <div className="max-w-screen-xl mx-auto ">
+          <section className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:gap-4">
+            <DisplayFurnituresSkeleton numSkeletons={numSkeletons} />
+          </section>
+        </div>
+      </section>
+    );
   }, []);
 
   const displayFurnitures = useCallback(
     (furnitures, imageUrls, loading) => {
-      if (loading) {
-        return displaySkeleton(10);
-      }
-
       return furnitures.map((furniture, index) => (
         <Link to={`item/${toSlug(furniture.name)}/${furniture.id}`} key={index}>
           <article className="relative cursor-pointer">
@@ -174,13 +175,18 @@ const DisplayFurnitures = () => {
     [displaySkeleton]
   );
 
+  if (isLoading) return displaySkeleton(10);
+  if (error) return <p>Error loading data...</p>;
+
+  const { furnitures, imageUrls } = data;
+
   return (
     <section className="bg-white md:pl-8 text-arfablack">
       <div className="max-w-screen-xl mx-auto ">
         <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:gap-4">
-          {displayFurnitures(furnitures, imageUrls, loading)}
+          {displayFurnitures(furnitures, imageUrls, isLoading)}
         </div>
-        {furnitures.length == 0 && !loading ? (
+        {furnitures.length === 0 && !isLoading && (
           <div className="flex flex-col items-center justify-center w-full gap-3 mt-10">
             <img src={noResult} className="w-60 h-60" />
             <h1 className="text-2xl font-semibold tracking-tight text-arfablack">
@@ -191,7 +197,7 @@ const DisplayFurnitures = () => {
               check back later.
             </p>
           </div>
-        ) : null}
+        )}
       </div>
     </section>
   );
