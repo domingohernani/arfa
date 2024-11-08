1c9542e8134739187a88faecfde3302926ff91f2
domingohernani <dev.hernani.domingo@gmail.com>
2024-11-09 01:27:53 +0800

    ui updating status

--- client/src/components/modals/OrderStatus.jsx
+++ client/src/components/modals/OrderStatus.jsx
@@ -5,7 +5,7 @@ import {
     DialogTitle,
     DialogPanel,
 } from "@headlessui/react";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
     ShoppingCartIcon,
     CheckCircleIcon,
@@ -16,6 +16,7 @@ import {
     ArrowUpTrayIcon,
 } from "@heroicons/react/24/outline";
 import { updateOrderStatus } from "../../firebase/orders";
+import { UploadPOD } from "./UploadPOD";
 
 const statusFlowOptions = {
     pickup: [
@@ -44,6 +45,9 @@ const getNextStatus = (currentStatus, statusFlow) => {
 
 export const OrderStatus = ({ refreshPage, isOpen, close, orderId, status, onDelivery, statusTimestamps, isOrderPage }) => {
     const statusFlow = onDelivery ? statusFlowOptions.delivery : statusFlowOptions.pickup;
+    const [isUploadPOD, setIsUploadPOD] = useState(false);
+    const [imagePOD, setImagePOD] = useState(null);
+    const [imagePODPreview, setPODPreview] = useState("");
 
     const handleUpdateStatus = async () => {
         if (status === "Picked-up" || status === "Delivered") {
@@ -53,6 +57,10 @@ export const OrderStatus = ({ refreshPage, isOpen, close, orderId, status, onDel
 
         try {
             const nextStatus = getNextStatus(status, statusFlow);
+            if (nextStatus === "Preparing") {
+                setIsUploadPOD(true);
+                return;
+            }
             if (nextStatus) {
                 await updateOrderStatus(orderId, nextStatus);
                 if (refreshPage) {
@@ -68,69 +76,84 @@ export const OrderStatus = ({ refreshPage, isOpen, close, orderId, status, onDel
         close();
     };
 
+    const handleImagePOD = (file) => {
+        if (file) {
+            setPODPreview(URL.createObjectURL(file));
+            setImagePOD(file);
+        }
+    };
+
     const handleCancel = () => {
         close();
     };
 
-    return (
-        <Transition appear show={isOpen} as={Fragment} className="z-40">
-            <Dialog as="div" className="relative z-10" onClose={close}>
-                <TransitionChild as={Fragment}>
-                    <div className="fixed inset-0 bg-black bg-opacity-30" />
-                </TransitionChild>
+    const handleClosePOD = () => {
+        setIsUploadPOD(false);
+    }
 
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex items-center justify-center min-h-full p-4 text-center">
-                        <TransitionChild
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
-                            <DialogPanel className="w-full max-w-xl p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
-                                <DialogTitle
-                                    as="h3"
-                                    className="text-lg font-medium leading-6 text-gray-900"
-                                >
-                                    {isOrderPage ? "Update Status" : "Order Status Logs"}
-                                </DialogTitle>
-                                <p className="text-sm text-gray-500 mt-1">
-                                    {isOrderPage
-                                        ? "Review the current order status and select the next stage in the process. This will help keep both the customer and the delivery team updated on the progress of the order."
-                                        : "This is a record of the completed stages in the order process, providing a history of updates for this transaction."}
-                                </p>
-                                <div>
-                                    <Tracking currentStatus={status} statusFlow={statusFlow} statusTimestamps={statusTimestamps} />
-                                </div>
-                                <div className="flex justify-between mt-4">
-                                    <button
-                                        type="button"
-                                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md bg-arfagreen hover:bg-green-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-opacity-75"
-                                        onClick={handleUpdateStatus}
+    return (
+        <>
+            <UploadPOD close={handleClosePOD} isOpen={isUploadPOD} upload={handleImagePOD} />
+            <Transition appear show={isOpen} as={Fragment} className="z-40">
+                <Dialog as="div" className="relative z-10" onClose={close}>
+                    <TransitionChild as={Fragment}>
+                        <div className="fixed inset-0 bg-black bg-opacity-30" />
+                    </TransitionChild>
+
+                    <div className="fixed inset-0 z-10 overflow-y-auto">
+                        <div className="flex items-center justify-center min-h-full p-4 text-center">
+                            <TransitionChild
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <DialogPanel className="w-full max-w-xl p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
+                                    <DialogTitle
+                                        as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900"
                                     >
-                                        Update
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-opacity-75"
-                                        onClick={handleCancel}
-                                    >
-                                        Cancel
-                                    </button>
-                                </div>
-                            </DialogPanel>
-                        </TransitionChild>
+                                        {isOrderPage ? "Update Status" : "Order Status Logs"}
+                                    </DialogTitle>
+                                    <p className="text-sm text-gray-500 mt-1">
+                                        {isOrderPage
+                                            ? "Review the current order status and select the next stage in the process. This will help keep both the customer and the delivery team updated on the progress of the order."
+                                            : "This is a record of the completed stages in the order process, providing a history of updates for this transaction."}
+                                    </p>
+                                    <div>
+                                        <Tracking currentStatus={status} statusFlow={statusFlow} statusTimestamps={statusTimestamps} previewPODUrl={imagePODPreview} />
+                                    </div>
+                                    <div className="flex justify-between mt-4">
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md bg-arfagreen hover:bg-green-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-opacity-75"
+                                            onClick={handleUpdateStatus}
+                                        >
+                                            Update
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-500 focus-visible:ring-opacity-75"
+                                            onClick={handleCancel}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </DialogPanel>
+                            </TransitionChild>
+                        </div>
                     </div>
-                </div>
-            </Dialog>
-        </Transition>
+                </Dialog>
+            </Transition>
+        </>
+
     );
 };
 
-const Tracking = ({ currentStatus, statusFlow, statusTimestamps }) => {
+const Tracking = ({ currentStatus, statusFlow, statusTimestamps, previewPODUrl }) => {
     const currentIndex = statusFlow.findIndex((s) => s.status === currentStatus);
     const statusesToShow = statusFlow.slice(0, currentIndex + 1).reverse();
 
@@ -139,6 +162,14 @@ const Tracking = ({ currentStatus, statusFlow, statusTimestamps }) => {
             <div className="mx-auto max-w-screen-xl 2xl:px-0">
                 <div className="lg:flex h-96 overflow-y-auto lg:gap-8">
                     <div className="grow sm:mt-8 lg:mt-0">
+                        {
+                            previewPODUrl && <section>
+                                <p className="text-sm text-arfablack">
+                                    Proof of Delivery
+                                </p>
+                                <img src={previewPODUrl} alt="Image Preview" className="w-full h-auto" />
+                            </section>
+                        }
                         <div className="space-y-6 rounded-lg bg-white p-4">
                             <ol className="relative ms-3 border-s border-gray-200 dark:border-gray-700">
                                 {statusesToShow.map((item, index) => (
