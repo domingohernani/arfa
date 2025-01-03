45191659b9b3f85d5249b4b3bc80352999b5caf8
Hernani Domingo <111575302+domingohernani@users.noreply.github.com>
2025-01-03 15:03:03 +0800

    Ar improved (#128)
    * margin
    
    * favicon
    
    * fixed validation bug
    
    * meta tag
    
    * add ruler icon and update ShowModel component for dimension selection
    
    * refactor ShowModel component to simplify dimension selection UI
    
    * add button to ShowModel component for user interaction

--- client/src/components/ShowModel.jsx
+++ client/src/components/ShowModel.jsx
@@ -343,36 +343,8 @@ function ShowModel({
           <line className="dimensionLine"></line>
         </svg>
 
-        <section className="flex flex-col gap-2 mt-4">
-          <div>
-            {variants.length != 0 && (
-              <div className="flex items-center">
-                <select
-                  id="variant"
-                  onChange={handleVariantChange}
-                  value={initialVariant}
-                  className="text-sm text-center border-none focus:border-transparent border-t-transparent border-x-transparent focus:ring-transparent focus:outline-none"
-                >
-                  {variants.map((variant, index) => (
-                    <option key={index} value={variant.value}>
-                      {variant.label}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            )}
-          </div>
-          <div>
-            <img src={ruler} className="w-5 h-5" onClick={showDimension} />
-            {/* <label htmlFor="show-dimensions">Dimensions:</label>
-            <input
-              id="show-dimensions"
-              type="checkbox"
-              checked={toggleDimension}
-              onChange={showDimension}
-              className="w-4 h-4 border-gray-300 rounded text-arfagreen focus:ring-arfagreen"
-            ></input> */}
-          </div>
+        <section>
+          <button className="bg-arfagray">Hello Po</button>
         </section>
       </model-viewer>
       <div className="flex items-center justify-between mx-auto my-2 text-sm controls">
